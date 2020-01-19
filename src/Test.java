edaba190c0684b68c598a093b9e6cc68e8c5b2dc
Saleh Omar <salehomar82@gmail.com>
2020-01-19 16:43:45 +0100

    Automaten Blatt 8 A3

--- src/Test.java
+++ src/Test.java
@@ -1,39 +1,47 @@
 
 public class Test {
-	
+
 	public static int pre(int n) {
-		if(n >= 1) {
+		if (n >= 1) {
 			return --n;
 		} else {
 			return 0;
 		}
 	}
-	
-	/*public static int add(int n, int m) {
-		if(n == 0) {
-			return m;
-		} else {
-			return (add(n, m) + 1);
-		}
-	}*/
-	
-	public static int diff(int m, int n) {
-		if(m == 0) {
-			return 0;
-		} else if(n == 0) {
-			return m;
-		} else {
-			return diff(pre(m), pre(n)); 	// In der Primitiven Rekursion wird für das Minus die im Skript definierte "pre()" funktion verwendet. 
-		}									// diff(m, n) = diff(pre(m), pre(n))
+
+	public static int add(int n, int m) {
+		return n + m;
 	}
-	
+
+	public static int diff(int m, int n) {
+		if (m == 0) {
+			return 0;
+		} else if (n == 0) {
+			return m;
+		} else {
+			return diff(pre(m), pre(n)); 	// In der Primitiven Rekursion wird für das Minus die im Skript definierte
+											// "pre()" funktion verwendet.
+		} 									// diff(m, n) = diff(pre(m), pre(n))
+	}
+
 	public static int absdiff(int m, int n) {
-		return (diff(m, n) + diff(n, m));
+		return (add(diff(m, n), diff(n, m)));
+	}
+
+	public static int odd(int n) {
+		if (n == 0) {
+			return 0;
+		} else if (n == 1) {
+			return 1;
+		} else {
+			return (diff(2, add(odd(diff(n, 1)), 1)));
+		}
 	}
 
 	public static void main(String[] args) {
-		System.out.println(diff(11,1));
+		System.out.println(diff(11, 1));
 		System.out.println(absdiff(3, 8));
+		System.out.println(odd(92));
 	}
 
 }
