c4c91a92a4dc1bb724db35fadea0e065d4893447
Saleh Omar <salehomar82@gmail.com>
2020-01-19 16:20:53 +0100

    Automaten Blatt 8 A2

--- src/Test.java
+++ src/Test.java
@@ -1,18 +1,39 @@
 
 public class Test {
 	
+	public static int pre(int n) {
+		if(n >= 1) {
+			return --n;
+		} else {
+			return 0;
+		}
+	}
+	
+	/*public static int add(int n, int m) {
+		if(n == 0) {
+			return m;
+		} else {
+			return (add(n, m) + 1);
+		}
+	}*/
+	
 	public static int diff(int m, int n) {
 		if(m == 0) {
 			return 0;
 		} else if(n == 0) {
 			return m;
 		} else {
-			return diff(m - 1, n - 1); 	// In der Primitiven Rekursion wird für das Minus(- 1) die im Skript definierte "pre()" funktion verwendet. 
-		}								// diff(m, n) = diff(pre(m), pre(n))
+			return diff(pre(m), pre(n)); 	// In der Primitiven Rekursion wird für das Minus die im Skript definierte "pre()" funktion verwendet. 
+		}									// diff(m, n) = diff(pre(m), pre(n))
+	}
+	
+	public static int absdiff(int m, int n) {
+		return (diff(m, n) + diff(n, m));
 	}
 
 	public static void main(String[] args) {
 		System.out.println(diff(11,1));
+		System.out.println(absdiff(3, 8));
 	}
 
 }
